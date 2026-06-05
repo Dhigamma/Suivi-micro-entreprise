e1830c72ffe17c1b622df424e0fce88c0845f20e
Dhigamma <william.bury@outlook.fr>
2026-06-05 10:05:06 +0200

    Delete src directory

--- src/Taches.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL || 'https://kupdycdxbjsxcippsdwq.supabase.co'
-const SUPABASE_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1cGR5Y2R4YmpzeGNpcHBzZHdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0Njk4MDMsImV4cCI6MjA5NjA0NTgwM30.MZZtKKAL-ES2OcIzDQi-rq2CdIyO2Y3B7j_y_be-SBE'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
